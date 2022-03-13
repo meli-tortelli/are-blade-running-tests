758a9c0970fcf3387d815922cc91df4f0c53471c
Melissa <melissa.tortelli@gmail.com>
2022-03-13 14:58:25 -0300

    casos de teste para shopping cart

---
+++ modulo-01/projeto-02/src/lib/Cart.spec.js
@@ -0,0 +1,106 @@
+import Cart from './Cart';
+
+describe('Cart', () => {
+  let cart;
+  let product = {
+    title: 'Adidas running shoes - men',
+    price: 35388,
+  };
+
+  let product2 = {
+    title: 'Adidas running shoes - women',
+    price: 41872,
+  };
+
+  beforeEach(() => {
+    cart = new Cart();
+  });
+
+  describe('getTotal()', () => {
+    it('should return 0 when getTotal() is executed in a newly created instance', () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it('should multiply quantity and price and receive the total amount', () => {
+      const item = {
+        product,
+        quantity: 2,
+      };
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(70776);
+    });
+
+    it('should ensure no more than one product exists at a time', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(35388);
+    });
+
+    it('should update total when a product gets included and then removed', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(41872);
+    });
+  });
+
+  describe('checkout()', () => {
+    it('should return an object with the total and the list of items', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 5,
+      });
+
+      expect(cart.checkout()).toMatchSnapshot();
+    });
+
+    it('should return an object with the total and the list of items when sumary() is called', () => {
+      cart.add({
+        product,
+        quantity: 2,
+      });
+
+      cart.add({
+        product: product2,
+        quantity: 5,
+      });
+
+      expect(cart.sumary()).toMatchSnapshot();
+      expect(cart.getTotal()).toBeGreaterThan(0);
+    });
+
+    it('should reset the cart when checkout is called', () => {
+      cart.add({
+        product: product2,
+        quantity: 5,
+      });
+
+      cart.checkout();
+
+      expect(cart.getTotal()).toEqual(0);
+    });
+  });
+});
